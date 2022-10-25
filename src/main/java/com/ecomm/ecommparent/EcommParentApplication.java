cafe69c78c8cc405e70faa496adea1857ef1469f
keshancp <keshanc.perera@gmail.com>
2022-10-25 11:46:45 +0530

    initial commit

---
+++ src/main/java/com/ecomm/ecommparent/EcommParentApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.ecommparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommParentApplication.class, args);
+	}
+
+}
